b64eec013332c15710d21dd446cfd7f6b0196ebd
mohammad blur <blor1379@gmail.com>
2024-05-21 20:01:45 +0330

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Mohammad Blur on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
