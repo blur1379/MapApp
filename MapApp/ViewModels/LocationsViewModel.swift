608f79f92d23f2ce62eae41e39e2d126a7ab7383
mohammad blur <blor1379@gmail.com>
2024-05-22 21:33:12 +0330

    :sparkles: add current location to location view model

--- MapApp/ViewModels/LocationsViewModel.swift
+++ MapApp/ViewModels/LocationsViewModel.swift
@@ -9,10 +9,15 @@ import Foundation
 
 class LocationsViewModel: ObservableObject {
     
+    // All loaded locations
     @Published var locations: [Location]
+    
+    // current location on map
+    @Published var mapLocations: Location
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocations = locations.first!
     }
 }
