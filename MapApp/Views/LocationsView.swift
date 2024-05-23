4e0ede2f1dce1721fecd18f0cfaea53be2c48977
mohammad blur <blor1379@gmail.com>
2024-05-23 22:59:28 +0330

    :sparkles: add header view

--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -16,6 +16,14 @@ struct LocationsView: View {
         ZStack {
             Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                
+                headerView()
+                    .padding()
+                
+                Spacer()
+            }
         }
     }
 }
@@ -23,4 +31,29 @@ struct LocationsView: View {
 #Preview {
     LocationsView()
         .environmentObject(LocationsViewModel())
+}
+
+extension LocationsView {
+    
+    @ViewBuilder
+    func headerView() -> some View {
+        VStack(spacing: 0) {
+            Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                .font(.title2)
+                .fontWeight(.black)
+                .foregroundColor(.primary)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .overlay(alignment: .leading) {
+                    Image(systemName: "arrow.down")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                        .padding()
+                }
+            LocationsListView()
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
 }
