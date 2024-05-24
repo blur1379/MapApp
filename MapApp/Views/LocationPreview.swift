502c7738c14bdbf9b7de73b19e5591f7ea53b0b7
mohammad blur <blor1379@gmail.com>
2024-05-24 23:43:49 +0330

    :lipstick: implement preview body

--- MapApp/Views/LocationPreview.swift
+++ MapApp/Views/LocationPreview.swift
@@ -12,7 +12,32 @@ struct LocationPreview: View {
     let location: Location
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            
+            ZStack {
+                if let imageName = location.imageNames.first {
+                    Image(imageName)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(10)
+                }
+            }
+            .padding(6)
+            .background(Color.red)
+            .cornerRadius(10)
+            
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(location.name)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                
+                Text(location.cityName)
+                    .font(.subheadline)
+            }
+            
+        }
     }
 }
 
